5d60dd36399e74b37d981be4e6a4f8e183c20b37
Kuprovskiy <oleg.kuprovskiy@gmail.com>
2026-01-24 21:02:59 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -48,7 +48,7 @@ export function Header() {
         <div className="flex items-center">
           <a href="/">
             <img src="/logo3.png" alt="Emirates Escape" className="h-14 object-contain" />
-          </>
+          </a>
         </div>
 
         {/* Right side controls */}
